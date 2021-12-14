c22bb784585056f32fdab9ddf90701c3772ba1e3
Adrian Nykiel <adrian@DESKTOP-E5OD8FS.localdomain>
2021-12-14 14:16:04 +0000

    favicon

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -8184,7 +8184,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46715" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40795" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
